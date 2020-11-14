8ff3b59487073df2ea7e181d6e490a5445ef6e77
amin havasi <pau.ahq@gmail.com>
2020-11-14 20:50:24 +0330

    design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from './components/auth/login';
+import Home from './components/homepage/home';
+import Register from './components/auth/register';
+const App = () => {
+    return (
+        <div>
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </div>
+    );
+};
 
 export default App;
